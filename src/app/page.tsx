8e6d5e47fe639af26f9fcd89c3a87d575239de7f
varYeon <xjkooy100@gmail.com>
2025-11-22 03:12:36 +0900

    style: nav UI 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Page() {
-  return <div>realtime</div>;
+  redirect("/rooms");
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ import { MessageCircleMoreIcon, User } from "lucide-react";
 
 export default function Nav() {
   const ButtonClass =
-    "h-10 w-40 rounded-xl bg-white/10 flex justify-center items-center hover:bg-gray-400";
+    "h-10 w-40 rounded-xl bg-white/10 flex justify-center items-center hover:bg-gray-400 shadow-xl";
   return (
     <div className="flex gap-10 justify-center absolute bottom-0 w-full mb-5">
       <div className={ButtonClass}>
