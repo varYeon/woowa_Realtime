dd874f649d3a1c9b335b7e9bd9995ee017284d2d
varYeon <xjkooy100@gmail.com>
2025-11-23 22:19:10 +0900

    feat: nav바 라우터 연결

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,26 @@
+"use client";
+
 import { MessageCircleMoreIcon, User } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function Nav() {
+  const router = useRouter();
   const ButtonClass =
     "h-10 w-40 rounded-xl bg-white/10 flex justify-center items-center hover:bg-gray-400 shadow-xl";
+
+  const clickRoomsHandler = () => {
+    router.push("/rooms");
+  };
+  const clickUsersHandler = () => {
+    router.push("/users");
+  };
+
   return (
     <div className="flex gap-10 justify-center absolute bottom-0 w-full mb-5">
-      <div className={ButtonClass}>
+      <div className={ButtonClass} onClick={clickRoomsHandler}>
         <MessageCircleMoreIcon />
       </div>
-      <div className={ButtonClass}>
+      <div className={ButtonClass} onClick={clickUsersHandler}>
         <User />
       </div>
     </div>
