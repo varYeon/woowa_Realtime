2fddf252b56e6641bbfde8ab63f06b58386cba07
varYeon <xjkooy100@gmail.com>
2025-11-24 03:37:51 +0900

    feat: 채팅창 입력 시 데이터 전송

--- src/components/messages/MessageBar.tsx
+++ src/components/messages/MessageBar.tsx
@@ -1,13 +1,44 @@
+import { createClient } from "@/utils/supabase/client";
 import { Send } from "lucide-react";
+import { useParams } from "next/navigation";
+import { useState } from "react";
 
 export default function MessageBar() {
+  const [message, setMessage] = useState("");
+  const { roomId } = useParams() as { roomId: string };
+
+  const messageHandler = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!message.trim()) return;
+
+    const supabase = await createClient();
+
+    await supabase.from("messages").insert([
+      {
+        room_id: roomId,
+        nickname: localStorage.getItem("nickname") ?? "",
+        content: message,
+      },
+    ]);
+
+    setMessage("");
+  };
+
   return (
-    <div className="flex fixed bottom-25 w-96 p-5 mx-2 rounded-xl bg-white/40 shadow-xl h-2 items-center">
+    <form
+      className="flex fixed bottom-25 w-96 p-5 mx-2 rounded-xl bg-white/40 shadow-xl h-2 items-center"
+      onSubmit={messageHandler}
+    >
       <input
         placeholder="Send your message ..."
         className="w-full p-2 outline-none"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
       ></input>
-      <Send />
-    </div>
+      <button type="submit">
+        <Send />
+      </button>
+    </form>
   );
 }
