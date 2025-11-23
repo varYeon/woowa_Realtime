1a24a350bc45e6d7120c6c4112b3f6c600b3ec91
varYeon <xjkooy100@gmail.com>
2025-11-23 22:28:39 +0900

    feat: 뒤로가기 라우터 연결

--- src/components/messages/Message.tsx
+++ src/components/messages/Message.tsx
@@ -1,11 +1,20 @@
+"use client";
+
 import { ChevronLeft } from "lucide-react";
 import MessageBubble from "./MessageBubble";
 import MessageBar from "./MessageBar";
+import { useRouter } from "next/navigation";
 
 export default function Message() {
+  const router = useRouter();
+
+  const backHandler = () => {
+    router.push("/rooms");
+  };
+
   return (
     <>
-      <div className="flex gap-3 p-2">
+      <div className="flex gap-3 p-2" onClick={backHandler}>
         <ChevronLeft className="cursor-pointer" />
         <span className="text-shadow-xs">nickname</span>
       </div>
