414d31d6c69511cb86ba20f6b2009dca20569541
varYeon <xjkooy100@gmail.com>
2025-11-24 01:10:48 +0900

    feat: 유저 닉네임 로컬 저장

--- src/components/users/NicknameInput.tsx
+++ src/components/users/NicknameInput.tsx
@@ -1,6 +1,16 @@
+"use client";
+
 import { Send } from "lucide-react";
+import { useState } from "react";
 
 export default function NicknameInput() {
+  const [nickname, setNickname] = useState("");
+
+  const submitHandler = () => {
+    if (!nickname.trim()) return alert("닉네임을 입력해주세요!");
+    localStorage.setItem("nickname", nickname);
+  };
+
   return (
     <>
       <div className="h-screen flex justify-center">
@@ -8,13 +18,20 @@ export default function NicknameInput() {
           <span className="flex flex-col items-center">
             Please enter your nickname.
           </span>
-          <div className="flex bottom-0 p-5 rounded-xl bg-white/40 shadow-xl h-2 items-center mb-70">
+          <form
+            className="flex bottom-0 p-5 rounded-xl bg-white/40 shadow-xl h-2 items-center mb-70"
+            onSubmit={submitHandler}
+          >
             <input
               placeholder="nickname"
               className="w-full p-2 outline-none"
+              value={nickname}
+              onChange={(e) => setNickname(e.target.value)}
             ></input>
-            <Send />
-          </div>
+            <button>
+              <Send type="submit" />
+            </button>
+          </form>
         </div>
       </div>
     </>
