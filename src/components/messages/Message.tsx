27670f377cae3803bf808be887b1eae15b1f6066
varYeon <xjkooy100@gmail.com>
2025-11-24 15:35:45 +0900

    feat: 개인 채팅방 상단바 닉네임 표시

--- src/components/messages/Message.tsx
+++ src/components/messages/Message.tsx
@@ -1,24 +1,14 @@
 "use client";
 
-import { ChevronLeft } from "lucide-react";
 import MessageBubble from "./MessageBubble";
 import MessageBar from "./MessageBar";
-import { useParams, useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
 
 export default function Message() {
-  const router = useRouter();
   const { roomId } = useParams() as { roomId: string };
-
-  const backHandler = () => {
-    router.push("/rooms");
-  };
 
   return (
     <div className="relative">
-      <div className="flex gap-3 p-2" onClick={backHandler}>
-        <ChevronLeft className="cursor-pointer" />
-        <span className="text-shadow-xs">nickname</span>
-      </div>
       <MessageBubble roomId={roomId} />
       <MessageBar roomId={roomId} />
     </div>
--- src/components/messages/MessageBubble.tsx
+++ src/components/messages/MessageBubble.tsx
@@ -3,9 +3,12 @@
 import { Message } from "@/types/Message";
 import { formattedMessage } from "@/utils/formatDate";
 import { createClient } from "@/utils/supabase/client";
+import { ChevronLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function MessageBubble({ roomId }: { roomId: string }) {
+  const router = useRouter();
   const [isMine, setIsMine] = useState(true);
   const [messages, setMessages] = useState<Message[]>([]);
 
@@ -14,6 +17,10 @@ export default function MessageBubble({ roomId }: { roomId: string }) {
   const contentClass = `${
     isMine ? "bg-indigo-900/40" : "bg-white/40"
   } max-w-2/3 min-h-10 rounded-b-xl rounded-l-xl p-2 text-sm break-words shadow-xl`;
+
+  const backHandler = () => {
+    router.push("/rooms");
+  };
 
   useEffect(() => {
     const fetchMessages = async () => {
@@ -41,6 +48,10 @@ export default function MessageBubble({ roomId }: { roomId: string }) {
     <>
       {messages.map((message) => (
         <>
+          <div className="flex gap-3 p-2" onClick={backHandler}>
+            <ChevronLeft className="cursor-pointer" />
+            <span className="text-shadow-xs">{message.nickname}</span>
+          </div>
           {/* notMine */}
           {!isMine && (
             <div key={message.id} className={bubbleClass}>
